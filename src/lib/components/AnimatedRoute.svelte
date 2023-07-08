647a196e913548eaf0c3e0efb15472ba6f59d6d4
Cooper Sheets <coopersheets@gmail.com>
2023-07-08 16:21:54 -0500

    Added login routes

---
+++ src/lib/components/AnimatedRoute.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    import { fly } from "svelte/transition";
+    import { page } from "$app/stores";
+</script>
+
+<!--subscribe to page url so that animation plays on change-->
+{#key $page.url}
+    <div in:fly={{ x: '-100%', duration: 500 }}>
+        <slot />
+    </div>
+{/key}
